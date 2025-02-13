bc9e4ed78171ef9d3f8f4df424e0936700fbf670
lmfaber <l-faber@posteo.de>
2025-02-13 14:08:43 +0100

    Update plugin information

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,44 @@
 <?php
 
-namespace Kanboard\Plugin\AutomaticAction;
+namespace Kanboard\Plugin\TaskAssignDueDateOnCreationByPriority;
 
 use Kanboard\Core\Plugin\Base;
-use Kanboard\Plugin\AutomaticAction\Action\TaskAssignDueDateOnCreationByPriority;
+use Kanboard\Plugin\TaskAssignDueDateOnCreationByPriority\Action\TaskAssignDueDateOnCreationByPriority;
 
 class Plugin extends Base
 {
     public function initialize()
     {
         $this->actionManager->register(new TaskAssignDueDateOnCreationByPriority($this->container));
+    }
+
+        public function getPluginName()
+    {
+        return 'TaskAssignDueDateOnCreationByPriority';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Assign a due date on task creation based on priority');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Lasse Faber';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/DKFZ-NGSCF/kanboard-TaskAssignDueDateOnCreationByPriority';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.43';
     }
 }
